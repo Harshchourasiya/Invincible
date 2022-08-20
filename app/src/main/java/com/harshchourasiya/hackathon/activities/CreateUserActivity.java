49a7a321a36f613ef535d77bdb93e7359307b6c8
harsh chourasiya <harshchourasiya130@gmail.com>
2022-08-20 20:27:46 +0530

    User Can Login, Modify The User Create Account to Convert to Firebase Authentication

--- app/src/main/java/com/harshchourasiya/hackathon/activities/CreateUserActivity.java
+++ app/src/main/java/com/harshchourasiya/hackathon/activities/CreateUserActivity.java
@@ -1,20 +1,29 @@
 package com.harshchourasiya.hackathon.activities;
 
+import static com.harshchourasiya.hackathon.MainActivity.TAG;
 import static com.harshchourasiya.hackathon.helper.DataFromSP.setUserIdInSp;
 import static com.harshchourasiya.hackathon.helper.Validator.isEmail;
 import static com.harshchourasiya.hackathon.helper.Validator.isName;
 import static com.harshchourasiya.hackathon.helper.Validator.isPassword;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.harshchourasiya.hackathon.MainActivity;
 import com.harshchourasiya.hackathon.R;
 import com.harshchourasiya.hackathon.helper.DatabaseUtility;
@@ -108,9 +117,31 @@ public class CreateUserActivity extends AppCompatActivity {
     }
 
     private void storeDataInDatabase(String email, String password, String name) {
-        String userId = UUID.randomUUID().toString();
-        new DatabaseUtility().AddUser(userId, email, password, name);
-        setUserIdInSp(this,userId);
+        authenticateUserUsingFirebaseAuth(email, password, name);
+    }
+
+    private void authenticateUserUsingFirebaseAuth(String email, String password, String name) {
+        FirebaseAuth mAuth =  FirebaseAuth.getInstance();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(CreateUserActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            successfullyUserIsCreated(mAuth.getCurrentUser(),
+                                    email,
+                                    password,
+                                    name);
+                        } else {
+                            Toast.makeText(CreateUserActivity.this, "Unable to create Account",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void successfullyUserIsCreated(FirebaseUser user, String email, String password,String name) {
+        new DatabaseUtility().AddUser(user.getUid(), email, password, name);
+        setUserIdInSp(CreateUserActivity.this,user.getUid());
         openMainActivity();
     }
 
--- app/src/main/java/com/harshchourasiya/hackathon/activities/LoginActivity.java
+++ app/src/main/java/com/harshchourasiya/hackathon/activities/LoginActivity.java
@@ -1,22 +1,41 @@
 package com.harshchourasiya.hackathon.activities;
 
+import static com.harshchourasiya.hackathon.helper.DataFromSP.setUserIdInSp;
+import static com.harshchourasiya.hackathon.helper.Validator.isEmail;
+import static com.harshchourasiya.hackathon.helper.Validator.isPassword;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.harshchourasiya.hackathon.MainActivity;
 import com.harshchourasiya.hackathon.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextInputLayout emailInputLayout, passwordInputLayout;
+    private TextInputEditText emailEditText, passwordEditText;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         setCreateUserButtonAction();
+        assignVariables();
+        setLoginButtonAction();
     }
+
 
     private void setCreateUserButtonAction() {
         findViewById(R.id.createButton).setOnClickListener(new View.OnClickListener() {
@@ -29,6 +48,63 @@ public class LoginActivity extends AppCompatActivity {
 
     private void openCreateUserActivity() {
         Intent intent = new Intent(this, CreateUserActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(intent);
+    }
+
+    private void assignVariables() {
+        emailInputLayout = findViewById(R.id.emailInputLayout);
+        passwordInputLayout = findViewById(R.id.passwordInputLayout);
+
+        emailEditText = findViewById(R.id.emailEditText );
+        passwordEditText = findViewById(R.id.passwordEditText);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void setLoginButtonAction() {
+        findViewById(R.id.loginButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validateUserData();
+            }
+        });
+    }
+
+    private void validateUserData() {
+        String email = emailEditText.getText().toString(), password = passwordEditText.getText().toString();
+
+        if (!isEmail(email)) {
+            emailInputLayout.setErrorEnabled(true);
+            return ;
+        }
+
+        if (!isPassword(password)) {
+            passwordInputLayout.setErrorEnabled(true);
+            return ;
+        }
+
+        authenticateUser(email, password);
+    }
+
+    private void authenticateUser(String email,String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            setUserIdInSp(LoginActivity.this,mAuth.getCurrentUser().getUid());
+                            openMainActivity();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
         startActivity(intent);
     }
