60d6299dc6cc956d520dc8c7151f8517d606cfb4
harsh chourasiya <harshchourasiya130@gmail.com>
2022-08-20 17:17:53 +0530

    Connect Firebase to Project, Upgrade dependencies and Add Internet permission

--- app/src/main/java/com/harshchourasiya/hackathon/MainActivity.java
+++ app/src/main/java/com/harshchourasiya/hackathon/MainActivity.java
@@ -3,12 +3,20 @@ package com.harshchourasiya.hackathon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static String TAG = "Test";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
